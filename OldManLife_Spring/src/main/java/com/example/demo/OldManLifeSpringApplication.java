251d7b7790d21f60d7799b8d10f8c939e8bb5bda
Unknown <will3shu@gmail.com>
2020-01-13 11:20:42 -0500

    Spring

---
+++ OldManLife_Spring/src/main/java/com/example/demo/OldManLifeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldManLifeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldManLifeSpringApplication.class, args);
+	}
+
+}
