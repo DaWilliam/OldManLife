3b97c98582633dfc881f8be4d77d61c50062e99d
Unknown <will3shu@gmail.com>
2020-01-15 14:38:50 -0500

    Populated with gifs and can end game

--- oldmanlife/src/Game.jsx
+++ oldmanlife/src/Game.jsx
@@ -3,16 +3,30 @@ import GifPlayer from 'react-gif-player';
 import './game.styles.css';
 
 //  This is a functional component that takes in the plot information and onclick to move the plot forward
-const Game = ({plot, optionOne, optionTwo, makeDecision}) => {
-    return <div className="game game-plot" >
-            <p>{plot}</p>    
-                            {/* Need this '() =>' calls it as a reference? */}        
-              <button className="game-button" onClick={() => makeDecision(optionOne)}>{ optionOne }</button>
-              <button className="game-button" onClick={() => makeDecision(optionTwo)}>{ optionTwo }</button>
-              <GifPlayer
-                  gif="https://media.giphy.com/media/l46CBvLI7Q7YthRzG/giphy.gif"
-                />
-            </div>
+const Game = ({plot, optionOne, optionTwo, gifURL, makeDecision, endGame}) => {
+    if(optionTwo === 'End')
+    {
+        return <div className="game game-plot" >
+                <p>{plot}</p>    
+                                {/* Need this '() =>' calls it as a reference? */}        
+                <button className="game-button" onClick={() => endGame(optionOne)}>{ optionOne }</button>
+                <button className="game-button" onClick={() => endGame(optionTwo)}>{ optionTwo }</button>
+                <GifPlayer
+                    gif={gifURL}
+                    />
+                </div>
+    }
+    else
+    {
+        return <div className="game game-plot" >
+                <p>{plot}</p>    
+                                {/* Need this '() =>' calls it as a reference? */}        
+                <button className="game-button" onClick={() => makeDecision(optionOne)}>{ optionOne }</button>
+                <button className="game-button" onClick={() => makeDecision(optionTwo)}>{ optionTwo }</button>
+                <GifPlayer
+                    gif={gifURL}/>
+                </div>
+    }
 }
 
 export default Game;
